ce104bd63d793677ad9c02be023746cf3dce3728
Ian Will <ian.will@nrl.navy.mil>
2012-10-13 21:35:21 -0400

    Finished 5.3.2 (rails routes)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
 
     it "should have the base title" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should have_selector('title', :text => "#{base_title}")
     end
 
     it "should not have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "#{base_title} | Home")
     end
   end
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the title 'Help'" do
-    	visit '/static_pages/help'
+    	visit help_path
     	page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
@@ -37,27 +37,26 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the title  'About'" do
-    	visit '/static_pages/about'
+    	visit about_path
     	page.should have_selector('title', :text=>"#{base_title} | About")
 	  end
   end
 
   describe "Contact page" do
   	it "should have the content 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_content('Contact')
 		end
 
 		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title', :text=>"#{base_title} | Contact")
 		end
   end
-
 
 end
